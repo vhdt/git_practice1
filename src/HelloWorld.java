4c89ae883d944ec3c6473bdebb0d73d456f34d7c
vhdt <v.kalinkaya@gmail.com>
2022-04-04 19:19:28 +0200

    work 2 is done

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloJava");
         System.out.println("HelloWorld");
+        //some work here 2
     }
 
 }
